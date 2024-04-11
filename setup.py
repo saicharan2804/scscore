1f3a2ceae00abc9311059438dbbd0ed7e962e3b0
saicharan2804 <saicharanhahaha@gmail.com>
2024-04-12 01:53:29 +0530

    changes to setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     description='A simple implementation of the SCScoring model',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/connorcoley/scscore',
+    url='https://github.com/saicharan2804/scscore',
     package_data={
-        'scscore': ['models/full_reaxys_model_1024bool/*', 'models/full_reaxys_model_2048uint/*'],
+        'scscore': ['scscore/models/full_reaxys_model_1024bool/*', 'scscore/models/full_reaxys_model_2048uint/*'],
     },
     install_requires=[
         'numpy',  # Add other dependencies as required
